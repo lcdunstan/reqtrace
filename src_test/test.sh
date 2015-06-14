ffa766c928baf7f7db2b93d1f65b0cecc2f51dff
Luke Dunstan <LukeDunstan81@gmail.com>
2015-06-14 23:23:05 +0800

    Disable building ppx_reqtrace

--- src_test/test.sh
+++ src_test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-ppx_reqtrace=$1
+reqtrace=$1
 indir=src_test
 outdir=_build/src_test
 
@@ -10,14 +10,14 @@ outdir=_build/src_test
 
 cp ${indir}/example_bad.ml ${outdir}/
 ocamlc -bin-annot -c ${outdir}/example_bad.ml
-if ./reqtrace.native extract --strip=_build/ ${outdir}/example_bad.cmt ; then
+if ${reqtrace} extract --strip=_build/ ${outdir}/example_bad.cmt ; then
     echo "Should have failed"
     exit 1
 fi
 
 cp -f ${indir}/example_req.ml ${outdir}/
 ocamlc -bin-annot -c ${outdir}/example_req.ml
-./reqtrace.native extract --strip=_build/ ${outdir}/example_req.cmt
+${reqtrace} extract --strip=_build/ ${outdir}/example_req.cmt
 ./python/rfc_notes.py ${indir}/example_spec.xml --ref=${outdir}/example_req.req --base=https://github.com/infidel/reqtrace/blob/master/ --html=${outdir}/example_spec.html
 grep coderef ${outdir}/example_spec.html
 
@@ -25,7 +25,7 @@ grep coderef ${outdir}/example_spec.html
 cp -f ${indir}/example_cstruct.ml ${outdir}/
 cstruct_dir=`ocamlfind query cstruct`
 ocamlfind ocamlc -package cstruct -syntax camlp4o -package cstruct.syntax -bin-annot -c ${outdir}/example_cstruct.ml
-./reqtrace.native extract --strip=_build/ --rfc mdns=6762 --rfc edns0=2671 ${outdir}/example_cstruct.cmt
+${reqtrace} extract --strip=_build/ --rfc mdns=6762 --rfc edns0=2671 ${outdir}/example_cstruct.cmt
 ./python/rfc_notes.py ${indir}/example_spec.xml --ref=${outdir}/example_cstruct.req --base=https://github.com/infidel/reqtrace/blob/master/ --html=${outdir}/example_cstruct.html
 grep coderef ${outdir}/example_cstruct.html
 
